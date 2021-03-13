d84c8f8ea90244c45c683ba5441cfe5e4f0a1a71
Alberto Clemente <alberto.clemente@sisal.it>
2021-03-13 18:26:24 +0100

    OCI Instance Module

--- instance.tf
+++ instance.tf
@@ -22,7 +22,7 @@ resource "oci_core_instance" "instance" {
 
   source_details {
     source_type = "image"
-    source_id = data.oci_core_images.image[0].id
+    source_id = data.oci_core_images.image.images[0].id
   }
   preserve_boot_volume = false
 
